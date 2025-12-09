b6d2b7ed0cad1ea5c02440603b2eb983689cb5ea
IsrarRashid <israrrashid3@gmail.com>
2025-12-09 13:04:16 +0500

    Update urdu content of generate complain pdf

--- app/utils/generateComplainPdf.ts
+++ app/utils/generateComplainPdf.ts
@@ -93,7 +93,7 @@ export const generateComplaintPDF = (item: ManageComplainsData) => {
         </div>
 
         <div class="main-heading">
-          از دفتر انچارج ڈی ایل کیو — پنجاب کنزیومر پروٹیکشن ایکٹ 2005
+          از دفتر اتھارٹی با تحت — پنجاب کنزیومر پروٹیکشن ایکٹ 2005
         </div>
 
         <div class="section">
@@ -120,13 +120,13 @@ export const generateComplaintPDF = (item: ManageComplainsData) => {
 
         <div>
           <p>
-            رخواست / عنوان با میں آپ کون ہار کیا جاتا ہے کہ آپ نے پنجاب کنزور پر ٹیکن ایکٹ
-            2005 کی دفعہ <span class="line">
+            درخواست / شکایت عنوان بالا میں آپ کو نوٹس ہذا جاری کیا جاتا ہے کہ آپ نے پنجاب کنزیومر پروٹیکشن ایکٹ
+            2005 کی دفعہ/دفعات <span class="line">
     ${
       item?.sectionsDetails
         ?.map((section) => section.name.replace("SECTION ", ""))
         .join(",") || ""
-    }</span>دفعات کی خلاف ورزی کی ہے۔ جس کی بابت آپ کے
+    }</span> کی خلاف ورزی کی ہے۔ جس کی بابت آپ کے
             خلاف کارروائی عمل میں لائی جارہی ہے۔
           </p>
         </div>
@@ -139,16 +139,16 @@ export const generateComplaintPDF = (item: ManageComplainsData) => {
                 ? format(toLocal(item.hearingDate), "dd-MM-yyyy")
                 : "--"
             }</span>
-            لوقت
+            بوقت
             <span class="line">${
               item?.hearingDate
                 ? format(toLocal(item.hearingDate), "hh:mm a")
                 : "--"
             }</span>
-            مام مالی تحفظ صارف کونسل <span class="line">  کنزیومر پروٹیکشن کونسل  </span>
-            اصالتا یا وکالتا حاضر ہوں یا کنزیومر پروٹیکٹ (Consumer Protect) اپ ڈاؤن لوڈ کر کے
-            اپنے کوائف رجسٹ کر لیں اور کارروائی بذریعہ ایکی سہولت لیں۔ بصورت دیگر کارروائی عمل
-            میں لائی جائے گی۔
+            بمقام ضلع تحفظ صارف کونسل <span class="line">  کنزیومر پروٹیکشن کونسل  </span>
+            اصالتا یا وکالتا حاضر ہوں یا کنزیومر پروٹیکٹ (Consumer Protect) ایپ ڈاؤن لوڈ کر کے
+            اپنے کوائف رجسٹر کر لیں اور کارروائی بذریعہ ایپ کی سہولت لیں۔ بصورت دیگر یک طرفہ کاروائی عمل
+            میں لائی جاوے گی۔
           </p>
         </div>
 
